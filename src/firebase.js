71db34d3a407f4ed71290707a475bafa66eadef7
Sanjaytholani <63097168+Sanjaytholani@users.noreply.github.com>
2021-02-04 11:28:39 +0530

    Day2 done

--- src/firebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCyAtdEXBMnXObdbG_HzZB4hOumssNkWw8",
-  authDomain: "netflix-68134.firebaseapp.com",
-  projectId: "netflix-68134",
-  storageBucket: "netflix-68134.appspot.com",
-  messagingSenderId: "385828888894",
-  appId: "1:385828888894:web:e2fa35619572160256cc6f",
-  measurementId: "G-ETQT8PRH9V",
-};
